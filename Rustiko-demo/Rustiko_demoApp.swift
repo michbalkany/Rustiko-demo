88301baa5ec795928ee0bc2d0f65204499695767
mich balkany <askmitchb@gmail.com>
2024-01-26 16:02:30 -0500

    Initial Commit

---
+++ Rustiko-demo/Rustiko_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rustiko_demoApp.swift
+//  Rustiko-demo
+//
+//  Created by Mich balkany on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Rustiko_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
