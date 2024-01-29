26abd64e393a67d52b5038fd9b77e53f191fc7d6
mich balkany <askmitchb@gmail.com>
2024-01-29 07:28:37 -0500

    content view, sideMenuHeaderView, SideMenueView, TabBar view not complete.

---
+++ Rustiko-demo/SideMenuHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuHeaderView.swift
+//  Rustiko-demo
+//
+//  Created by Mich balkany on 1/28/24.
+//
+
+import SwiftUI
+
+struct SideMenuHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuHeaderView()
+}
---
+++ Rustiko-demo/SideMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuView.swift
+//  Rustiko-demo
+//
+//  Created by Mich balkany on 1/28/24.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuView()
+}
